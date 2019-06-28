87d938e0e41a910c1ff46ca7e99ca4519c9c99dd
Alejandro <alejandro.gon.per@gmail.com>
2019-06-28 14:08:00 +0200

    Scenes and middleware

--- cosificaBOT/index.js
+++ cosificaBOT/index.js
@@ -1,10 +1,23 @@
 const Telegraf = require('telegraf');
 const TelegrafInlineMenu = require('telegraf-inline-menu');
+const session = require('telegraf/session');
+const Stage = require('telegraf/stage');
+const Scene = require('telegraf/scenes/base');
+const { leave } = Stage;
 
 
 
+
+// Greeter scene
+const greeter = new Scene('greeter');
+greeter.enter((ctx) => ctx.reply('Bienvenidx'));
+ctx.scene.enter('frame');
+
+
+// Frame scene
+const frame = new Scene('frame');
+frame.enter((ctx) => menu.replyMenuMiddleware());
 const menu = new TelegrafInlineMenu(ctx => `Hey ${ctx.from.first_name}!`);
-menu.setCommand('start');
 
 let options = ['Troceada', 'Objeto', 'Intercambiable','Maltrato', 'Sexualizada', 'Mercancía','Lienzo'];
 let selectionStatus = [];
@@ -26,11 +39,25 @@ menu.simpleButton('⏩Enviar⏪', 'c', {
 
 
 
+// Create scene manager
+const stage = new Stage();
+stage.command('cancel', leave());
+
+// Scene registration
+stage.register(greeter);
+stage.register(frame);
+
+
+
 
 ///////////////////// INIT BOT /////////////////////
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
+bot.use(session());
+bot.use(stage.middleware());
 bot.use(menu.init());
+
+
+bot.command('start', (ctx) => ctx.scene.enter('greeter'));
 
 bot.startPolling();
 
